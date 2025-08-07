8e3fbcf273678f4ec0eb63c1c2497d5c5e7dfc36
Naufal Fahmi <naufalffadhillah@gmail.com>
2025-08-07 20:56:30 +0700

    fix map

--- kebakaran.py
+++ kebakaran.py
@@ -350,7 +350,7 @@ with tab4:
 with tab5:
     with st.container():
         st.markdown("<style>iframe {background-color: white;}</style><h2 class='sub-header'>Peta</h2>", unsafe_allow_html=True)
-        st.components.v1.iframe(src="https://datawrapper.dwcdn.net/oSBOq/1/", height=907, scrolling=False)
+        st.components.v1.iframe(src="https://datawrapper.dwcdn.net/oSBOq/1/", height=908, scrolling=False)
 
 # Menambahkan footer
 st.markdown("<div class='footer'>© 2025 Analisis Keselamatan Kebakaran Jakarta | Sumber data: Dinas Pemadam Kebakaran Jakarta</div>", unsafe_allow_html=True)
